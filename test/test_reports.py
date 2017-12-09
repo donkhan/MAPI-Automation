92cd84f20173d94fdea52d2868f3482e7dc0597c
Sumitha Vasanthan <sumitha.v.r@gmail.com>
2017-12-09 12:42:29 +0530

    updating latest changes in test-reports

--- test/test_reports.py
+++ test/test_reports.py
@@ -3,6 +3,7 @@ import warnings
 import datetime
 import auth
 import com
+import pytest
 
 warnings.filterwarnings("ignore")
 headers = {}
@@ -26,7 +27,17 @@ def test_user_report_with_out_role():
 def test_content_type():
     response = com.post("/reports/user", headers,
                         data={'from': "2017-1-1", 'to': '2018-1-1', 'type': 'csv', 'role': 'financeManager'})
-    assert response.headers['Content-Type'] == 'text'
+
+    # assert response.headers['Content-Type'] == 'text'
+
+    @pytest.mark.parametrize("role", [
+        "customer",
+        "financeManager"
+    ])
+    def test_role(role):
+        response = com.post("/reports/user", headers,
+                            data={'from': "2017-1-1", 'to': '2018-1-1', 'type': 'csv', 'role': role})
+        assert response.status_code == 200
 
 
 def test_user_report_with_role(role):
@@ -104,18 +115,18 @@ def assert_role(content,role):
 def main():
     global headers
     headers = {"Api-Key":auth.auth()}
-    # test_content_type()
-    # test_user_report_with_out_role()
-    # test_user_report_with_role('financeManager')
-    # test_user_report_with_role('customer')
-    # test_user_report_with_junk_to()
-    # test_user_report_with_junk_from()
-    # test_user_report_with_invalid_dates()
-    # test_user_report_with_out_date()
-    # test_user_report_with_out_from_date()
-    # test_user_report_with_out_to_date()
-    # test_user_report_with_invalid_from_date()
-    # test_user_report_with_invalid_to_date()
+    test_content_type()
+    test_user_report_with_out_role()
+    test_user_report_with_role('financeManager')
+    test_user_report_with_role('customer')
+    test_user_report_with_junk_to()
+    test_user_report_with_junk_from()
+    test_user_report_with_invalid_dates()
+    test_user_report_with_out_date()
+    test_user_report_with_out_from_date()
+    test_user_report_with_out_to_date()
+    test_user_report_with_invalid_from_date()
+    test_user_report_with_invalid_to_date()
     test_user_report_with_invalid_dateFormat()
 
 

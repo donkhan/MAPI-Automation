b79aa89a362d3131c3c6a318a651437e5d21865e
kamilkhan@rediffmail.com <donkhan>
2018-01-14 20:32:06 +0530

    Direct user creation is added

--- test/test_user_direct_creation.py
+++ test/test_user_direct_creation.py
@@ -1,32 +1,39 @@
-
 import time
 import auth
 import requests
 import c
+import com
 
 headers = {}
 
 
 def setup_module(module):
     global headers
-    headers = {"Api-key":auth.auth()}
+    headers = {"Api-key": auth.auth()}
+
+
+def get_files():
+    front = open('/tmp/a.png')
+    back = open('/tmp/a.png')
+    files = {
+        'front': front, 'back': back
+    }
+    return files
 
 
 def test_user_create_direct():
-    front = open('/tmp/a.png')
-    back = open('/tmp/a.png')
-    print front
-    name = 'teste' + str(int(round(time.time() * 1000))) + "@maxmoney.com"
+    name = 'test_e' + str(int(round(time.time() * 1000))) + "@maxmoney.com"
     url = "/users/create-user"
-    data = {'idNo': 321234561112,'username': name,'name': name,
-                             'password': 'junk','mobile': '+919845104104',
-                             'role': 'maxCddOfficer','email' : name
-                             }
-    files = {
-        'front' : front,'back' : back
-    }
-    response = requests.post(c.site + url, verify=False, headers=headers, data=data,files= files)
-    print response
+    data = {'idNo': 321234561112, 'username': name, 'name': "Test User",
+            'password': 'junk', 'mobile': '+919845104104',
+            'role': 'maxCddOfficer', 'email': name
+            }
+    files = get_files()
+    response = requests.post(c.site + url, verify=False, headers=headers, data=data, files=files)
+    assert response.status_code == 200
+    files = get_files()
+    response = requests.post(c.site + url, verify=False, headers=headers, data=data, files=files)
+    assert response.status_code == 409
 
-#setup_module(None)
+
 test_user_create_direct()
